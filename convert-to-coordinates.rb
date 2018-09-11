0b60593b39b408e8bd97a57beccdf94795aa8ec7
Bharg Sharma <bharg.sharma@rea-group.com>
2018-09-11 23:23:09 +1000

    Remove bing api key from being published to GitHub

--- convert-to-coordinates.rb
+++ convert-to-coordinates.rb
@@ -1,9 +1,7 @@
 require 'geokit'
 require 'byebug'
 
-Geokit::Geocoders::BingGeocoder.key = 'AgUttEhFOtU8gt_GQAc37Pui_3y3YEWEMyrXeHNL3OVPfAvgeaz-JE8ekQou_-yf'
-
-def location_to_coordinates(address)    
+def location_to_coordinates(address)
     address.to_s
     coordinates = Geokit::Geocoders::BingGeocoder.geocode(address, :bias => 'aus')
     coordinates.ll
