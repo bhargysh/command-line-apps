b65be8093ddefc8f328c99fa496dd9937866e616
Bharg Sharma <bharg.sharma@rea-group.com>
2018-10-16 22:43:59 +1100

    Retrieves current temp via get request

--- current_temp.rb
+++ current_temp.rb
@@ -1,15 +1,26 @@
+require 'net/http'
+require 'uri'
+require 'byebug'
+
 class Temperature
   def initialize
     @lat = Coordinates.lat
     @long = Coordinates.long
   end
 
-  def self.fetch_current_temp
-    fetch_response
+  def fetch_current_temp(lat, long)
+    fetch_response(lat, long)
   end
 
   private
   def fetch_response(lat, long)
-    curl "api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&APPID=weather_api_key"
+    #curl "http://api.openweathermap.org/data/2.5/weather?lat=25&lon=55&APPID=weather_api_key"
+    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&APPID=weather_api_key")
+    response = Net::HTTP.get(uri)
+    extract_temp(response)
+  end
+
+  def extract_temp(response)
+    temp = response['main']['temp']
   end
 end
--- spec/current_temp_spec.rb
+++ spec/current_temp_spec.rb
@@ -1,15 +1,23 @@
 require './current_temp'
-RSpec.describe do
-  subject(:fetch_current_temp) do
-    described_class.fetch_current_temp
+require './address_to_coordinates'
+RSpec.describe Temperature do
+  let(:lat) { 25 }
+  let(:long) { 55 }
+  subject(:current_temp) do
+    described_class.new.fetch_current_temp(lat, long)
   end
 
   context 'when correct lat and long is given' do
-    let(:lat) { 25 }
-    let(:long) { 55 }
-    let(:response) { '25 degrees' }
+    let(:coordinates) { '25, 55' }
+    let(:response) { '25 deg' }
 
-    it 'returns the correct current temperature' do
+    before do
+      allow(Coordinates).to receive(:address_to_coordinates).and_return(coordinates)
+      #instance_double(Temperature)
+      allow(Temperature).to receive(:fetch_response).and_return(response)
+    end
+
+    it 'returns the current temperature' do
       expect(subject).to eq(response)
     end
   end
