124d1643498034bd07738a08b508ccaea8869c9d
Bharg Sharma <bharg.sharma@rea-group.com>
2019-02-10 17:42:35 +1100

    Remove stuff not needed

--- app/geocoder.rb
+++ app/geocoder.rb
@@ -2,7 +2,7 @@ require 'geokit'
 
 class Geocoder
   def self.bing_api_key
-    Geokit::Geocoders::BingGeocoder.key = 'AgUttEhFOtU8gt_GQAc37Pui_3y3YEWEMyrXeHNL3OVPfAvgeaz-JE8ekQou_-yf'
+    Geokit::Geocoders::BingGeocoder.key = ''
   end
   def self.coordinates(address)
     coordinates = Geokit::Geocoders::BingGeocoder.geocode(address, :bias => 'aus')
